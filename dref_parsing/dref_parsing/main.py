2e62f8c3467aab125c064c76b02291b2328fe912
Alex Howes <howesalexandra@gmail.com>
2024-01-25 11:15:27 +0100

    Rename old endpoint and add new endpoint using new classes

--- dref_parsing/dref_parsing/main.py
+++ dref_parsing/dref_parsing/main.py
@@ -50,19 +50,71 @@ async def run_parsing(
         if not results[lead]:
             compare = all_parsed.compare(all_parsed_new)
             print(compare)
-    
-    if all(results.values()):
-        print(f"\n\nPASS for codes: {list(results.keys())}")
-    else:
-        failed_mdr_codes = [code for code in results if not(results[code])]
-        passed_mdr_codes = [code for code in results if results[code]]
-        print(f"\n\nPASS for codes: {failed_mdr_codes}")
-        print(f"\n\nFAIL for codes: {passed_mdr_codes}")
+            
+    failed_mdr_codes = [code for code in results if not(results[code])]
+    passed_mdr_codes = [code for code in results if results[code]]
+    print(f"\n\nPASS for codes: {failed_mdr_codes}")
+    print(f"\n\nFAIL for codes: {passed_mdr_codes}")
     
     return all(results)
 
 
 @app.post("/parse/")
+async def run_parsing(
+    appeal_code: str = Query(
+        default=None,
+        title="Appeal code",
+        description="Starts with 'MDR' followed by 5 symbols. <br> Some available codes: DO013, BO014, CL014, AR017, VU008, TJ029, SO009, PH040, RS014, FJ004, CD031, MY005, LA007, CU006, AM006",
+        min_length=8,
+        max_length=8,
+        required=True
+        )
+    ):
+    """
+    App for Parsing PDFs of DREF Final Reports.   
+    <b>Input</b>: Appeal code of the report, MDR*****  
+    <b>Output</b>:  
+    &nbsp;&nbsp; a dictionary of excerpts extracted from the PDF with its features: 'Learning', 'DREF_Sector',  
+    &nbsp;&nbsp; and global features: 'Hazard', 'Country', 'Date', 'Region', 'Appeal code'
+
+    The app uses IFRC GO API to determine the global features (call 'appeal')  
+    and to get the URL of the PDF report (call 'appeal_document')
+
+    <b>Possible errors</b>:  
+    <ul>
+    <li> Appeal code doesn't have a DREF Final Report in IFRC GO appeal database 
+    <li> PDF URL for Appeal code was not found using IFRC GO API call appeal_document
+    <li> PDF Parsing didn't work
+    </ul>
+    """    
+    try:
+        appeal = Appeal(mdr_code=appeal_code)
+        appeal_data = pd.DataFrame([{
+            'lead': appeal_code,
+            'Hazard': appeal.official_hazard_name, 
+            'Country': appeal.country,
+            'Region': appeal.region,
+            'Date': appeal.start_date
+        }])
+        appeal_document = appeal.get_dref_final_report()
+        exs_parsed, _ = appeal_document.get_challenges_lessons_learned()
+        all_parsed = exs_parsed.merge(appeal_data, on='lead')
+    except ExceptionNotInAPI:
+        raise HTTPException(status_code=404, 
+                            detail=f"{appeal_code} doesn't have a DREF Final Report in IFRC GO appeal database")
+    except ExceptionNoURLforPDF:
+        raise HTTPException(status_code=404, 
+                            detail=f"PDF URL for Appeal code {appeal_code} was not found using IFRC GO API call appeal_document")
+    except:
+        raise HTTPException(status_code=500, detail="PDF Parsing didn't work by some reason")
+
+    df = all_parsed[['Modified Excerpt', 'Learning', 'DREF_Sector', 'lead', 'Hazard', 'Country', 'Date', 'Region']]
+    df = df.rename(columns={'lead': 'Appeal code', 'Modified Excerpt': 'Excerpt'})
+
+    return df.to_dict()
+
+
+@app.post("/parse_old/")
 async def run_parsing(
     Appeal_code: str = Query(
         'MDRDO013',
