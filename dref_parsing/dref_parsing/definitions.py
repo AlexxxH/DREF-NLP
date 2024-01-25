f7f18c5091ce2f6ecaf5a2187b5b3225bc079889
Alex Howes <howesalexandra@gmail.com>
2024-01-25 03:21:37 +0100

    Add sectors to definitions

--- dref_parsing/dref_parsing/definitions.py
+++ dref_parsing/dref_parsing/definitions.py
@@ -20,3 +20,80 @@ DREF_FINAL_REPORT_NAMES = [
   'DREF Final Report',
   'DREF Operation Final Report 1'
 ]
+SECTORS = [
+  {
+    'name': 'Health',
+    'id': 'Health',
+    'true name': True
+  },
+  {
+    'name': 'Education',
+    'id': 'Education',
+    'true name': True
+  },
+  {
+    'name': 'Shelter and Settlements',
+    'id': 'Shelter',
+    'true name': True
+  },
+  {
+    'name': 'Disaster Risk Reduction and Climate Action',
+    'id': 'Disaster',
+    'true name': True
+  },
+  {
+    'name': 'Water Sanitation and Hygiene',
+    'id': 'WASH',
+    'true name': True
+  },
+  {
+    'name': 'Livelihoods and Basic Needs',
+    'id': 'Live',
+    'true name': True
+  },
+  {
+    'name': 'Strategies for implementation',
+    'id': 'Strategies',
+    'true name': True
+  },
+  {
+    'name': 'Protection, Gender and Inclusion',
+    'id': 'PGI',
+    'true name': True
+  },
+  {
+    'name': 'Migration and Displacement',
+    'id': 'Migration',
+    'true name': True
+  },
+  {
+    'name': 'National Society Strengthening',
+    'id': 'Strategies',
+    'true name': False
+  },
+  {
+    'name': 'National Society Capacity',
+    'id': 'Strategies',
+    'true name': False
+  },
+  {
+    'name': 'Strengthen National Society',
+    'id': 'Strategies',
+    'true name': False
+  },
+  {
+    'name': 'Strategies for Implementation',
+    'id': 'Strategies',
+    'true name': False
+  },
+  {
+    'name': 'International Disaster Response',
+    'id': 'Strategies',
+    'true name': False
+  },
+  {
+    'name': 'Influence others as leading strategic',
+    'id': 'Strategies',
+    'true name': False
+  }
+]
